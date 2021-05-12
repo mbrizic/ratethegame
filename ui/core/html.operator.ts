2d7745ea9a3bf82b5d73fbaa6bd70156eef92ace
mbrizic <brizic.mario@gmail.com>
2021-05-13 01:30:05 +0200

    Add ability to execute JS on client side, but only as a "progressive enhancement" for better UX

--- ui/core/html.operator.ts
+++ ui/core/html.operator.ts
@@ -1,3 +1,16 @@
 export function Inline(...elements: string[]) {
     return elements.join("")
 }
+
+// Takes some JS, executes it and puts execution result into span element
+export function ExecuteJsAndDisplayResult(code: string) {
+    return `<span>
+                <script>
+                    document.currentScript.parentElement.textContent = ${code.trim()}
+                </script>
+            </span>`
+}
+
+export function ExecuteJs(code: string) {
+    return `<script>${code}; document.currentScript.remove()</script>`
+}
