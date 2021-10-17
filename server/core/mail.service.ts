92fbe23f8c66a257a925d4158e7a024867b26138
mbrizic <brizic.mario@gmail.com>
2021-10-18 00:14:53 +0200

    Add email notifications service

---
+++ server/core/mail.service.ts
@@ -0,0 +1,62 @@
+import { getAppConfig } from "./app.config";
+import * as https from 'https';
+
+const { mailClientApiKey, mailClientSenderEmail } = getAppConfig()
+
+export function sendEmail(recipientEmail: string, text: string) {
+    const body = {
+        personalizations: [{
+            to: [ {
+                email: recipientEmail,
+                name: recipientEmail     
+            }],
+        }],
+        from: {
+            email: mailClientSenderEmail,
+            name: "Rate the Game mailer"      
+        },
+        subject: "Your RTG report",
+        content: [
+          {
+            type: "text/html",
+            value: `
+                <h1>Your RTG report</h1>
+                <p>${text}</p>
+                <a href="http://ratethegame.supercollider.hr/">unsubscribe here</a>
+            `
+          }
+        ],
+      }
+
+    const data = new TextEncoder().encode(
+        JSON.stringify(body)
+    )
+    
+    const options = {
+        hostname: 'api.sendgrid.com',
+        port: 443,
+        path: '/v3/mail/send',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length,
+            'Authorization': `Bearer ${mailClientApiKey}`
+        }
+    }
+    
+    const request = https.request(options, (res: any) => {
+        console.log('Mailer: ', res.statusCode)
+        res.on('data', (d: any) => {
+            process.stdout.write(d)
+        })
+    })
+    
+    request.on('error', (error: any) => {
+        console.error(error)
+    })
+    
+    request.write(data)
+    request.end()
+}
+
+
