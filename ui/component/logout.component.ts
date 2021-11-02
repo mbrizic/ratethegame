835a634adbbdba286f28210bca23392dbed2de0d
mbrizic <brizic.mario@gmail.com>
2021-11-02 02:34:31 +0100

    Update styles

--- ui/component/logout.component.ts
+++ ui/component/logout.component.ts
@@ -1,12 +1,12 @@
 import { returnUrlQueryParam } from "../../server/core/constants"
-import { Form, SubmitButton } from "../core/html.elements"
+import { Form, SubmitButton, SubmitLink } from "../core/html.elements"
 import { SimpleComponent } from "../core/html.interfaces"
 import { ExecuteJs, Inline } from "../core/html.operator"
 
 export const Logout: SimpleComponent = () => {
     return Inline(
         Form(`/logout`,
-            SubmitButton("Log out")
+            SubmitLink("Log out")
         ),
         ExecuteJs(`
             let form = document.querySelectorAll("form[action*='/logout']")[0]
